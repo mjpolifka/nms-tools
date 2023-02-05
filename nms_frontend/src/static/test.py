c83d54f874745399fa8370eb1ef58e7cc1fb8655
Mitch Polifka <mjpolifka@gmail.com>
2023-02-05 16:06:14 -0500

    continuing refactor

--- nms_frontend/src/static/test.py
+++ nms_frontend/src/static/test.py
@@ -19,38 +19,29 @@ def cascade_ingredients(item):
 
     ingredients = {}
 
-    if item in data.keys(): # This should be a try/catch block for readability
+    try:
+    #if item in data.keys(): # This should be a try/catch block for readability
         # read its ingredients
         ings = data[item]['ingredients']
         for i in ings.keys():
             # Check if the ingredient has ingredients
             if i not in data.keys():
                 ingredients = save_ingredient(i, ings, ingredients)
-                '''
-                # If it doesn't, save it
-                if i in ingredients.keys():
-                    # If this ingredient is already on the list, add to it
-                    ingredients[i] = ingredients[i] + ings[i]
-                else:
-                    # Otherwise add it to the list (is there a python function that can simplify this?)
-                    ingredients[i] = ings[i]
-                '''
             else:
                 # If it does, call the function again and save the return value
                 c = cascade_ingredients(i)
                 for j in c:
                     ingredients = save_ingredient(j, c, ingredients)
-                    '''
-                    if j in ingredients.keys():
-                        ingredients[j] = ingredients[j] + c[j]
-                    else:
-                        ingredients[j] = c[j]
-                    '''
+    except KeyError as e:
+        print("Key error: " + e)
+        return 0
+    '''
     else:
         # This should probably be a try/catch
         print("item '" + item + "' not found, stopping")
         # I don't think this should ever happen, but it'll probably throw an error if it does
         return 0
+    '''
 
     return ingredients
 
