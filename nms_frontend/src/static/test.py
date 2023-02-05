1263ee9697f7b748bf0e1e4b15cd013d51eed6b3
Mitch Polifka <mjpolifka@gmail.com>
2023-02-05 16:03:55 -0500

    started refactor in test.py

--- nms_frontend/src/static/test.py
+++ nms_frontend/src/static/test.py
@@ -9,63 +9,57 @@ def cascade_ingredients(item):
     '''
     - take an item
     - read its ingredients
-    - check if the ingredient is baseline
-    - if it is, save it
-    - if it's not, call the function again and save the return value
+    - check if the ingredient has ingredients
+    - if it doesn't, save it
+    - if it does, call the function again and save the return value
     - return the full list of ingredients
     '''
 
+    # Take an item (given to the function)
+
     ingredients = {}
 
-    if item in data.keys():
+    if item in data.keys(): # This should be a try/catch block for readability
+        # read its ingredients
         ings = data[item]['ingredients']
         for i in ings.keys():
-            if i in data.keys():
-                # print("recursive trigger")
-                # print("calling cascade_ingredients on '" + i + "'")
+            # Check if the ingredient has ingredients
+            if i not in data.keys():
+                ingredients = save_ingredient(i, ings, ingredients)
+                '''
+                # If it doesn't, save it
+                if i in ingredients.keys():
+                    # If this ingredient is already on the list, add to it
+                    ingredients[i] = ingredients[i] + ings[i]
+                else:
+                    # Otherwise add it to the list (is there a python function that can simplify this?)
+                    ingredients[i] = ings[i]
+                '''
+            else:
+                # If it does, call the function again and save the return value
                 c = cascade_ingredients(i)
-                #print(c)
                 for j in c:
-                    # print(str(j) + ": " + str(c[j]))
+                    ingredients = save_ingredient(j, c, ingredients)
+                    '''
                     if j in ingredients.keys():
                         ingredients[j] = ingredients[j] + c[j]
                     else:
                         ingredients[j] = c[j]
-                # call the function again, but first I want to test that it breaks properly
-            else:
-                if i in ingredients.keys():
-                    ingredients[i] = ingredients[i] + ings[i]
-                else:
-                    ingredients[i] = ings[i]
+                    '''
     else:
+        # This should probably be a try/catch
         print("item '" + item + "' not found, stopping")
         # I don't think this should ever happen, but it'll probably throw an error if it does
         return 0
 
     return ingredients
-    '''
-    ingredients = {}
 
-    while True:
-        if item in data.keys():
-            print("item '" + item + "' in data.keys")
-            l = data[item]['ingredients']
-            print("ingredients: " + str(l))
-            for i in l.keys():
-                print("trying '" + str(i) + "'")
-                if i not in data.keys():
-                    print(i + " is in data.keys")
-                    if i in ingredients.keys():
-                        ingredients[i] = ingredients[i] + l[i]
-                    else:
-                        ingredients[i] = l[i]
-
-        else:
-            print("item '" + item + "' not found, stopping")
-            break
-
+def save_ingredient(i, j, ingredients):
+    if i in ingredients.keys():
+        ingredients[i] = ingredients[i] + j[i]
+    else:
+        ingredients[i] = j[i]
     return ingredients
-    '''
 
 print("Poly Fibre: " + str(cascade_ingredients('poly_fibre')))
 print("Quantum Processor: " + str(cascade_ingredients('quantum_processor')))
